73a8bc2f6852d3be17437f27dc088d1f0dab6be7
Hardil Desai <hardil.bd@gmail.com>
2025-09-19 18:40:00 +0000

    this is laggy wtf

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -4,11 +4,7 @@ import Lenis from '@studio-freight/lenis';
 
 const SmoothScroll = () => {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothTouch: true,
-    });
+    const lenis = new Lenis();
 
     function raf(time: number) {
       lenis.raf(time);
