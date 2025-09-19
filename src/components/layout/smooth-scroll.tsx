4f6bc9be3dbc0269cbb0bffbb606be795beaf73c
Hardil Desai <hardil.bd@gmail.com>
2025-09-19 18:47:08 +0000

    const lenis = new Lenis({   duration: 1.2,   easing: (t) => Math.min(1,

---
+++ src/components/layout/smooth-scroll.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+};
+
+export default SmoothScroll;
