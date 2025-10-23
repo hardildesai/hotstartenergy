da81945ab4cdce8e3b656f68c2f234eb2bb97d16
Hardil Desai <hardil.bd@gmail.com>
2025-10-23 06:44:36 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/logos/marine-electricals.tsx
+++ src/components/logos/marine-electricals.tsx
@@ -14,7 +14,7 @@ export function MarineElectricalsLogo({ className }: { className?: string }) {
       <path d="M30 30V0h12c8.3 0 15 6.7 15 15s-6.7 15-15 15H30zm8-22v14h4c4.1 0 7.5-3.1 7.5-7s-3.4-7-7.5-7h-4z" />
       <path d="M60 30V0h8l6 12V0h8v30h-8l-6-12v12h-8z" />
       <path d="M85 30V0h8v30h-8z" />
-      <path dM100 30V0h12c8.3 0 15 6.7 15 15s-6.7 15-15 15h-12zm8-22v14h4c4.1 0 7.5-3.1 7.5-7s-3.4-7-7.5-7h-4z" />
+      <path d="M100 30V0h12c8.3 0 15 6.7 15 15s-6.7 15-15 15h-12zm8-22v14h4c4.1 0 7.5-3.1 7.5-7s-3.4-7-7.5-7h-4z" />
       <path d="M130 30V0h8v12h12V0h8v30h-8V18h-12v12h-8z" />
       <path d="M160 30V0h8v12h12V0h8v30h-8V18h-12v12h-8z" />
     </svg>
