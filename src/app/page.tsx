798152a24457f553cabee2ae8fbf2b6cafa9486d
Hardil Desai <hardil.bd@gmail.com>
2025-10-23 08:46:32 +0000

    Create a modern “About / What We Do” section for the homepage of Hotstar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { AboutSection } from '@/components/home/about-section';
 import { Hero } from '@/components/home/hero';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
+      <AboutSection />
     </div>
   );
 }
