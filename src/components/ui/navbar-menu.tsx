6c8880f55e339cdda8c9a4d0d94f906c150c9d7d
Hardil Desai <hardil.bd@gmail.com>
2025-11-02 14:08:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -97,23 +97,25 @@ export const ProductItem = ({
   setActive: (item: string | null) => void;
 }) => {
   return (
-    <Link href={href} className="flex space-x-2" onClick={() => setActive(null)}>
-      <Image
-        src={src}
-        width={60}
-        height={60}
-        alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl object-cover"
-      />
-      <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
-      </div>
-    </Link>
+    <div className="flex space-x-2">
+      <Link href={href} className="flex space-x-2" onClick={() => setActive(null)}>
+        <Image
+          src={src}
+          width={60}
+          height={60}
+          alt={title}
+          className="flex-shrink-0 rounded-md shadow-2xl object-cover"
+        />
+        <div>
+          <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+            {title}
+          </h4>
+          <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
+            {description}
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 };
 
